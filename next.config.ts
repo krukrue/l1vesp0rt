b7be226cf7a5ca9c9a92569511e3460b5f24f0b0
Kvashnin-firstis <kvashnin23@gmail.com>
2026-02-16 22:04:06 +0100

    init app settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.livesport.cz",
+        pathname: "/res/image/data/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
